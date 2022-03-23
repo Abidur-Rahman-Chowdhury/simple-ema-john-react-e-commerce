7075eacfc4b49277a92b618b8172ecf37da8838b
Abidur Rahman <abid43iiuc@gmail.com>
2022-03-23 12:58:48 +0600

    done cart component pricing tax shipping

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from "react";
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const { cart } = props;
+    let total = 0;
+    let shipping = 0;
+    
+    for (const product of cart) {
+        total += product.price;
+        shipping += product.shipping;
+        
+    }
+    const tax = total * 0.1;
+    console.log(total);
+    console.log(cart);
   return (
-    <div>
+    <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected items: {cart.length}</p>
+          <p>Selected items: {cart.length}</p>
+          <p>Total Price: ${total} </p>
+          <p>Total Shipping: ${shipping} </p>
+          <p>Tax:{Number(tax.toFixed(2))} </p>
+          <p><strong>Grand Total: </strong></p>
     </div>
   );
 };
