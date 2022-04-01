c390592327534bdac06410a0cf393464842d0302
Abidur Rahman <abid43iiuc@gmail.com>
2022-04-01 11:35:54 +0600

    done orders inventory component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,30 +1,34 @@
 import React from "react";
-import './Cart.css'
+import "./Cart.css";
 
 const Cart = (props) => {
-    const { cart } = props;
-    
-    let total = 0;
-    let shipping = 0;
-    let quantity = 0;
-    
-    for (const product of cart) {
-        quantity = quantity + product.quantity;
-        total += product.price * product.quantity;
-        shipping += product.shipping * product.quantity;
-        
-    }
-    const tax = (total * 0.1).toFixed(2);
-    const grandTotal = total + shipping + Number(tax);
-    
+  const { cart } = props;
+
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total += product.price * product.quantity;
+    shipping += product.shipping * product.quantity;
+  }
+  const tax = (total * 0.1).toFixed(2);
+  const grandTotal = total + shipping + Number(tax);
+
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-          <p>Selected items: {quantity}</p>
-          <p>Total Price: ${total} </p>
-          <p>Total Shipping: ${shipping} </p>
-          <p>Tax:{tax} </p>
-          <p><strong>Grand Total: ${grandTotal} </strong></p>
+      <p>Selected items: {quantity}</p>
+      <p>Total Price: ${total} </p>
+      <p>Total Shipping: ${shipping} </p>
+      <p>Tax:{tax} </p>
+      <p>
+        <strong>Grand Total: ${grandTotal} </strong>
+      </p>
+      {
+        props.children
+      }
     </div>
   );
 };
