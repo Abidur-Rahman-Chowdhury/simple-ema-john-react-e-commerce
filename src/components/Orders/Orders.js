e224e5fcda5a963a4ac1575c00b59138cb11b874
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-24 01:53:25 +0600

    replace .id to ._id

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -5,35 +5,37 @@ import useProducts from '../../hooks/useProduct';
 import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import './Orders.css'
+import './Orders.css';
 
 const Orders = () => {
-    const [products, setProducts] = useProducts();
-    const [cart, setCart] = useCart(products);
-    const handelRemoveProduct = (product) => {
-        const rest = cart.filter(pd => pd.id !== product.id);
-        setCart(rest);
-        removeFromDb(product.id)
-    }
-    const navigate = useNavigate();
-    return (
-        <div className='shop-container'>
-
-            <div className='review-items-container'>
-                {
-                    cart.map(product => <ReviewItem key={product._id} product={product} handelRemoveProduct={handelRemoveProduct}></ReviewItem>)
-                }
-            </div>
-            <div className='cart-container'>
-
-                <Cart cart={cart}>
-                   
-                        <button onClick={()=> navigate('/shipment')}>Proceed Shipping</button>
-                    
-                </Cart>
-            </div>
-        </div>
-    );
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
+  const handelRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd._id !== product._id);
+    setCart(rest);
+    removeFromDb(product._id);
+  };
+  const navigate = useNavigate();
+  return (
+    <div className="shop-container">
+      <div className="review-items-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product._id}
+            product={product}
+            handelRemoveProduct={handelRemoveProduct}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <button onClick={() => navigate('/shipment')}>
+            Proceed Shipping
+          </button>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,61 +6,61 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
-    const [products, setProducts] = useProducts();
-    const [cart,setCart] = useState([]);
-    
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useState([]);
 
-    useEffect(() => {
-        const storedCart = getStoredCart();
-        const saveCart = [];
-        for (const id in storedCart) {
-            const addedProduct = products.find(product => product.id === id)
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                saveCart.push(addedProduct);
-            }
-            
-        }
-        setCart(saveCart);
-    },[products])
-   
-    const handelAddToCart = (SelectedProduct) => {
-        //  confusing part see next time carefully
-        // do not do this cart.push(product)
-        let newCart = [];
-        const exists = cart.find(product => product.id === SelectedProduct.id);
-        if (!exists) {
-            SelectedProduct.quantity = 1;
-            newCart = [...cart, SelectedProduct];
-        } else {
-            const rest = cart.filter(product => product.id !== SelectedProduct.id);
-            exists.quantity = exists.quantity + 1;
-            newCart = [...rest, exists];
-
-        }
-        setCart(newCart);
-        addToDb(SelectedProduct.id);
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    const saveCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product._id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        saveCart.push(addedProduct);
+      }
     }
-    return (
-        <div className='shop-container'>
-            
-            <div className='products-container'>
-                {
-                    products.map(product => <Product key={product._id} product ={product} handelAddToCart ={handelAddToCart}
-                    
-                    ></Product>)
-                }
-            </div>
-            <div className='cart-container'>
-                <Cart cart={cart}>
-                    <Link to='/orders'>
-                        <button>Review Order</button>
-                    </Link>
-                </Cart>
-            </div>
-        </div>
-    );
+    setCart(saveCart);
+  }, [products]);
+
+  const handelAddToCart = (SelectedProduct) => {
+    //  confusing part see next time carefully
+    // do not do this cart.push(product)
+    let newCart = [];
+    const exists = cart.find((product) => product._id === SelectedProduct._id);
+    if (!exists) {
+      SelectedProduct.quantity = 1;
+      newCart = [...cart, SelectedProduct];
+    } else {
+      const rest = cart.filter(
+        (product) => product._id !== SelectedProduct._id
+      );
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
+    setCart(newCart);
+    addToDb(SelectedProduct._id);
+  };
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            key={product._id}
+            product={product}
+            handelAddToCart={handelAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <Link to="/orders">
+            <button>Review Order</button>
+          </Link>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
